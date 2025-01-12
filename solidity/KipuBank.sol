b8358595d70aed69a45d842bfce7f26b9c8b4a6a
Antonio Quental <antonio.quental@gmail.com>
2025-01-12 13:16:28 -0300

    updated with corrections suggestions

--- solidity/KipuBank.sol
+++ solidity/KipuBank.sol
@@ -2,36 +2,53 @@
 pragma solidity ^0.8.19;
 
 contract KipuBank {
-    uint256 public constant MAX_WITHDRAWAL = 1 ether; // Limite máximo por saque (1 ETH)
-    uint256 public immutable bankCap; // Limite máximo de ETH no banco
+    uint256 public constant c_MAX_WITHDRAWAL = 1 ether; // Limite máximo por saque (1 ETH)
+    uint256 public immutable i_bankCap; // Limite máximo de ETH no banco
 
-    mapping(address => uint256) public balances; // Mapeamento de endereços para saldos
+    mapping(address => uint256) public s_balances; // Mapeamento de endereços para saldos
+    address public immutable i_owner; // endereço do portador dos fundos
 
     event Deposit(address indexed _from, uint256 _value);
     event Withdrawal(address indexed _from, uint256 _value);
 
-    constructor(uint256 _bankCap) {
-        bankCap = _bankCap;
+    constructor(uint256 _i_bankCap) {
+        i_bankCap = _i_bankCap;
+        i_owner = msg.sender;
     }
 
-    function deposit() public payable {
+    function kipubank_deposit() public payable {
         require(
-            msg.value + address(this).balance <= bankCap,
+            msg.value + address(this).balance <= i_bankCap,
             "Deposit exceeds bank cap"
         );
-        balances[msg.sender] += msg.value;
+        s_balances[msg.sender] += msg.value;
         emit Deposit(msg.sender, msg.value);
     }
 
-    function withdraw(uint256 _amount) public {
-        require(balances[msg.sender] >= _amount, "Insufficient balance");
-        require(_amount <= MAX_WITHDRAWAL, "Withdrawal exceeds maximum limit");
-        balances[msg.sender] -= _amount;
-        payable(msg.sender).transfer(_amount);
+    function _safeTransfer(uint256 _amount, address _to) internal {
+        (bool success, ) = payable(_to).call{value: _amount}(""); // realiza a transferência via call
+        require(success, "Transfer failed");
+    }
+
+    function kipubank_withdraw(uint256 _amount) public {
+        require(s_balances[msg.sender] >= _amount, "Insufficient balance");
+        require(
+            _amount <= c_MAX_WITHDRAWAL,
+            "Withdrawal exceeds maximum limit"
+        );
+        s_balances[msg.sender] -= _amount;
+        _safeTransfer(_amount, msg.sender);
         emit Withdrawal(msg.sender, _amount);
     }
 
-    function getBalance(address _account) public view returns (uint256) {
-        return balances[_account];
+    function kipubank_getBalance(
+        address _account
+    ) public view returns (uint256) {
+        require(
+            (msg.sender == _account || msg.sender == i_owner),
+            "Only owner or account holder can view balance"
+        );
+        return s_balances[_account];
     }
+    //TODO: implement modifier
 }
