a4fa75cfe3a3908980e1d89bf0b0aeaa9001f605
Antonio Quental <antonio.quental@gmail.com>
2025-03-18 19:30:42 -0300

    added comments to aquental-erc20.sol

--- solidity/aquental-erc20.sol
+++ solidity/aquental-erc20.sol
@@ -9,15 +9,31 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @custom:security-contact antonio.quental@gmail.com
 contract AQuental is ERC20, ERC20Burnable, Ownable, ERC20Permit {
-    constructor(address initialOwner)
-        ERC20("AQuental", "QNT")
-        Ownable(initialOwner)
-        ERC20Permit("AQuental")
+    // Defines the AQuental contract inheriting from multiple OpenZeppelin contracts:
+    // ERC20: Basic token functionality
+    // ERC20Burnable: Ability to burn tokens
+    // Ownable: Ownership and access control
+    // ERC20Permit: Permit functionality for gasless approvals
+    constructor(
+        address initialOwner
+    )
+        // Constructor takes initial owner address as parameter
+        ERC20("AQuental", "QNT") // Initializes ERC20 with name "AQuental" and symbol "QNT"
+        Ownable(initialOwner) // Sets the initial owner through Ownable constructor
+        ERC20Permit("AQuental") // Initializes permit functionality with token name
     {
         _mint(msg.sender, 10 * 10 ** decimals());
+        // Mints initial supply of 10 tokens (adjusted by decimals) to the deployer
+        // decimals() is inherited from ERC20 and typically returns 18
+        // So this creates 10 * 10^18 = 10 quintillion wei units
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+        // Public function to mint new tokens
+        // onlyOwner modifier restricts access to contract owner only
+        // Parameters:
+        // - to: address that will receive the newly minted tokens
+        // - amount: number of tokens to mint (in wei units)
+        _mint(to, amount); // Internal ERC20 function to create new tokens
     }
 }
